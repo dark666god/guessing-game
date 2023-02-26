d1cf983f764839f416e50fb09e3eec693b49e9af
Vitaliy Chistyakov <dark666god@yandex.ru>
2023-02-26 14:23:01 +0300

    fead:done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.floor(this.min + (this.max +1 - this.min)/2);
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
+
 
 module.exports = GuessingGame;
